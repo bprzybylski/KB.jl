fcf624230981a36874da7b023fdab7fb46a7a418
kalmarek <kalmar@amu.edu.pl>
2019-01-18 11:17:04 +0100

    Restructure build.jl; I have no idea why build fails...

--- deps/build.jl
+++ deps/build.jl
@@ -10,25 +10,29 @@ version = "1.5.6"
 provides(Sources, URI("https://github.com/gap-packages/kbmag/releases/download/v$version/kbmag-$version.tar.gz"), fsalib, unpacked_dir="kbmag-$version")
 
 src_dir = joinpath(BinDeps.depsdir(fsalib), "src")
+standalone_lib_dir = joinpath(src_dir, "kbmag-$version", "standalone", "lib")
 dst_dir = joinpath(BinDeps.depsdir(fsalib), "usr", "lib")
-@show dst_dir
-@show isfile(joinpath(dst_dir, "fsalib.$(Libdl.dlext)"))
+target_lib = joinpath(dst_dir, "fsalib.$(Libdl.dlext)")
 
 if Sys.isapple()
     nothing
 end
 
-provides(SimpleBuild, (@build_steps begin
-    GetSources(fsalib)
-    CreateDirectory(dst_dir)
-    @build_steps begin
-      FileRule([joinpath(dst_dir, "fsalib.$(Libdl.dlext)")], 
-        @build_steps begin
-          ChangeDirectory(joinpath(src_dir, "kbmag-$version", "standalone", "lib"))
-          `make fsalib.so`
-          `mv fsalib.$(Libdl.dlext) $dst_dir`
+fsalib_build =     @build_steps begin
+        GetSources(fsalib)
+        CreateDirectory(dst_dir)
+        FileDownloader(
+            "https://raw.githubusercontent.com/gap-packages/kbmag/12a09ba0b9aefa9e4817c9531ac22b9f0b9f3996/standalone/lib/makefile",
+            joinpath(standalone_lib_dir, "makefile.new"))
+
+        FileRule(target_lib, @build_steps begin
+            ChangeDirectory(standalone_lib_dir)
+            `mv makefile.new makefile`
+            MakeTargets("fsalib.so")
+            `mv fsalib.$(Libdl.dlext) $dst_dir`
         end)
     end
-  end), fsalib)
+
+provides(SimpleBuild, fsalib_build, fsalib)
 
 @BinDeps.install Dict(:fsalib => :fsalib)
