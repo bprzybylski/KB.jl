8b817ada4f9421a4a328a56004a34161b493e687
Bartek Przybylski <bprzybylski@users.noreply.github.com>
2019-03-14 21:07:33 +0100

    doc dates update

--- src/types.jl
+++ src/types.jl
@@ -21,7 +21,7 @@ end
     Enum name: StorageType
     Original enum name: storage_type
     Source: ./deps/src/kbmag-1.5.6/standalone/lib/fsa.h [40]
-    Date: 2019-03-12
+    Date: 2019-03-14
 =#
 @enum StorageType begin
     DENSE
@@ -33,7 +33,7 @@ end
     Enum name: KBMFlagStrings
     Original enum name: kbm_flag_strings
     Source: ./deps/src/kbmag-1.5.6/standalone/lib/fsa.h [41--50]
-    Date: 2019-03-12
+    Date: 2019-03-14
 =#
 @enum KBMFlagStrings begin
     DFA
