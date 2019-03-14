f15c9d0e6d04b3343c7e2d8dd8960a131271f8a7
Bartek Przybylski <bprzybylski@users.noreply.github.com>
2019-03-14 18:55:44 +0100

    SrecType doc update

--- src/types.jl
+++ src/types.jl
@@ -1,7 +1,7 @@
 export Fsa
 
 #=
-    Enum name: srec_type
+    Enum name: SrecType
     Original enum name: srec_type
     Source: ./deps/src/kbmag-1.5.6/standalone/lib/ [29--38]
     Date: 2019-03-12
