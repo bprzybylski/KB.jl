0d2c1286c6669a96d9d37e980b179003984b404e
Bartek Przybylski <bprzybylski@users.noreply.github.com>
2019-03-16 12:24:15 +0100

    struct RewritingSystem (and its little friends)

--- src/types.jl
+++ src/types.jl
@@ -47,6 +47,21 @@ end
 end
 
 #=
+    Enum name: KBMOrderings
+    Original enum name: kbm_orderings
+    Source: ./deps/src/kbmag-1.5.6/standalone/lib/rws.h [15-22]
+    Date: 2019-03-16
+=#
+@enum KBMOrderings begin
+    SHORTLEX
+    RECURSIVE
+    RT_RECURSIVE
+    WTLEX
+    WREATHPROD
+    NONE
+end
+
+#=
     Type name: Char
     Date: 2019-03-14
 =#
@@ -143,4 +158,127 @@ struct FSA
     is_accessible::Ptr{Bool}
     flags::Ptr{Bool} # Originally boolean flags[num_kbm_flag_strings] (what to do about that?)
     table::Ptr{TableStruc}
+end
+
+#=
+    Struct name: WDR
+    Original struct name: wdr
+    Source: ./deps/src/kbmag-1.5.6/standalone/lib/rws.h [115--119]
+    Date: 2019-03-16
+=#
+struct WDR
+    num_eqns::Int
+    num_states::Int
+    num_diff::Int
+end
+
+#=
+    Struct name: ReductionEquation
+    Original struct name: reduction_equation
+    Source: ./deps/src/kbmag-1.5.6/standalone/lib/rws.h [34--40]
+    Date: 2019-03-16
+=#
+struct ReductionEquation
+    lhs::Ptr{Gen}
+    rhs::Ptr{Gen}
+    done::Bool
+    changed::Bool
+    eliminated::Bool
+end
+
+#=
+    Struct name: RewritingSystem
+    Original struct name: rewriting_system
+    Source: ./deps/src/kbmag-1.5.6/standalone/lib/rws.h [42--160]
+    Date: 2019-03-16
+=#
+struct RewritingSystem
+    name::Ptr{Char} # Originally char name[256] (what to do about that?)
+    ordering::KBMOrderings
+    weight::Ptr{Int}
+    level::Ptr{Int}
+    confluent::Bool
+    num_gens::Int
+    num_eqns::Int
+    num_inveqns::Int
+    inv_of::Ptr{Int}
+    gen_name::Ptr{Ptr{Char}}
+    eqns::Ptr{ReductionEquation}
+    reduction_fsa::Ptr{FSA}
+    num_states::Int
+    worddiffs::Bool
+    wd_fsa::Ptr{FSA}
+    cosets::Bool
+    wd_reorder::Bool
+    new_wd::Ptr{Bool}
+    maxlenleft::Int
+    maxlenright::Int
+    maxoverlaplen::Int
+    hadlongoverlap::Bool
+    rkminlen::Int
+    rkmineqns::Int
+    rk_on::Bool
+    history::Ptr{Int}
+    slowhistory::Ptr{Ptr{Int}}
+    slowhistorysp::Ptr{Int}
+    preflen::Ptr{Int}
+    prefno::Ptr{Int}
+    maxpreflen::Int
+    outputprefixes::Bool
+    testword1::Ptr{Gen}
+    testword2::Ptr{Gen}
+    sorteqns::Bool
+    tidyint::Int
+    eqn_no::Ptr{Int}
+    nneweqns::Int
+    tot_eqns::Int
+    hadct::Int
+    maxhad::Int
+    maxoplen::Int
+    print_eqns::Bool
+    maxeqns::Int
+    hadmaxeqns::Bool
+    confnum::Int
+    oldconfnum::Int
+    maxslowhistoryspace::Int
+    maxreducelen::Int
+    maxstates::Int
+    current_maxstates::Int
+    double_states::Bool
+    init_fsaspace::Int
+    maxwdiffs::Int
+    exit_status::Int
+    tidyon::Bool
+    tidyon_now::Bool
+    wd_record::Ptr{WDR}
+    num_cycles::Int
+    eqn_factor::Int
+    states_factor::Int
+    halting_factor::Int
+    min_time::Int
+    halting::Bool
+    do_conf_test::Bool
+    lostinfo::Bool
+    resume::Bool
+    resume_with_orig::Bool
+    tidyintset::Bool
+    maxeqnsset::Bool
+    maxstatesset::Bool
+    orderingset::Bool
+    silentset::Bool
+    verboseset::Bool
+    confnumset::Bool
+    maxreducelenset::Bool
+    maxwdiffset::Bool
+    separator::Int
+    maxlhsrellen::Int
+    maxsubgenlen::Int
+    maxcosetlen::Int
+    finitestop::Bool
+    Hoverlaps::Bool
+    Gislevel::Bool
+    Hislevel::Bool
+    Hhasinverses::Bool
+    wd_alphabet::Ptr{Srec}
+    subwordsG::Ptr{Ptr{Gen}}
 end
