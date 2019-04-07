309df862cedcafd2dd30894c3b1eb3a0f05ef878
kalmarek <kalmar@amu.edu.pl>
2019-04-07 02:54:34 +0200

    second (different) read_kbinput

--- deps/patches/kbmag-1.5.6/standalone/lib/fsalib_wrapper.c
+++ deps/patches/kbmag-1.5.6/standalone/lib/fsalib_wrapper.c
@@ -12,3 +12,14 @@
 
 /* The following line was imported from the ../src/kbprog.c file */
 boolean kbm_onintr = FALSE;   /* set true on interrupt signal */
+
+int read_rws(char *infile, boolean check, rewriting_system *rws) {
+    FILE *rfile;
+    if ((rfile = fopen(infile, "r")) == 0){
+        return 1;
+    } else {
+        read_kbinput(rfile, check, rws);
+        fclose(rfile);
+    };
+    return 0;
+};
--- tmp.jl
+++ tmp.jl
@@ -7,16 +7,49 @@ using KB
 msg = "hi!"
 ccall((:stringlen, KB.fsalib), Cint, (Cstring,), msg)
 
-rws = KB.RewritingSystem()
-
+function name(rws::RewritingSystem)
+    k = findfirst(i -> i == 0, rws.name)
+    k == 1 && return ""
+    return String(reinterpret(UInt8, collect(rws.name[1:k-1])))
+end
 
 
 begin
     kb_data_dir = joinpath("deps", "src", "kbmag-1.5.6", "standalone", "kb_data")
 
     kb_examples = readdir(kb_data_dir)
-    ex = joinpath(kb_data_dir, kb_examples[1])
+    example_file = joinpath(kb_data_dir, kb_examples[14])
+    lines = open(ex, "r") do f
+        readlines(f)
+    end
+
+    println(join(lines, "\n"))
 end
 
-RewritingSystem(ex, rws)
+rws = KB.RewritingSystem()
+name(rws)
+RewritingSystem(example_file, rws)
+name(rws)
+@show rws;
 
+function read_rws(filename, rws; check=false)
+    rws_ptr = Base.unsafe_convert(Ptr{KB.RewritingSystem}, Ref(rws))
+    r = ccall((:read_rws, KB.fsalib),
+        Cint,
+        (Cstring, Bool, Ptr{KB.RewritingSystem}),
+        filename, false, rws_ptr)
+
+    r == 0 || error("Error while reading $filename.")
+
+    return rws
+end
+
+rws = KB.RewritingSystem()
+name(rws)
+read_rws(example_file, rws)
+name(rws)
+@show rws;
+
+
+read_rws(ex, rws)
+@show rws;
