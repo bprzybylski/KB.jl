913921012cfa36e6eeae6fea6768189d74edee19
Bartek Przybylski <bprzybylski@users.noreply.github.com>
2019-07-05 14:16:13 +0200

    Package structure update

--- src/KBmag.jl
+++ src/KBmag.jl
@@ -5,12 +5,10 @@ using Libdl
 if isfile(joinpath(dirname(@__FILE__), "..", "deps", "deps.jl"))
     include("../deps/deps.jl")
 else
-    error("KBmag not properly installed. Please (re) build KBmag and restart julia")
+    error("KBmag not properly installed. Please (re) build KBmag and restart Julia")
 end
 
-include("types.jl")
-include("RWS.jl")
-
-include("debug.jl")
+# Wrapping functions
+include("BinWrapper.jl")
 
 end # module
